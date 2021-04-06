433c905072478a4744aed79f6b1eca2d681d885d
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-04-06 11:30:41 -0400

    commit tree cf326bc965dc226a2ef634b02177dc252652301c

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,26 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |c|
+      if c.presence
+        self.notes.build(content: c)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.collect {|c| c.content}
+  end
 end
